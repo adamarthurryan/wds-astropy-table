0a79e98d91f11525d48933193bf9e65045048d35
Adam Brown <adamarthurryan@gmail.com>
2025-11-19 17:54:07 -0400

    finished spec of 6th orbital data

--- wds_astropy_table/sixth_orbital.py
+++ wds_astropy_table/sixth_orbital.py
@@ -31,37 +31,38 @@ sixth_orbital_columns = [
                                                 # y = years
                                                 # c = centuries (rarely used)
     (95, 105, 'f', 'period_error'),         # Published formal error in P (in same units as for P). 
-    (106, 115, 'f', 'semi_major'),           # Semi-major axis (a)
-    (116,116, 's', 'semi_major_units'),           # code for semi-major axis units:
+    (106, 114, 'f', 'semi_major'),           # Semi-major axis (a)
+    (115,115, 's', 'semi_major_units'),           # code for semi-major axis units:
                                                 # a = arcseconds
                                                 # m = milliarcseconds (mas)
                                                 # M = arcminutes (used only for alp Cen + Proxima Cen)
                                                 # u = microarcseconds (uas - not yet used)
-    (117, 125, 'f', 'semi_major_error'),     # Error in a. Units are the same as for a.
-#   13    T126,F8.4        Inclination (i), in degrees.
-#   14    T135,F8.4        Error in i.
-#   15    T144,F8.4,A1     Node (Omega), in degrees. An identified ascending node 
-#                          is indicated by an asterisk following the value. If 
-#                          the ascending node is later determined to off by 
-#                          180deg, it is flipped, and a "q" code added to 
-#                          indicate the change. 
-#   16    T154,F8.4        Error in Omega.
-#   17    T163,F12.6,A1    The time of periastron passage (T0) and code for units:
-#                            c = centuries (fractional year / 100; used only for 
-#                                alp Cen + Proxima Cen) 
-#                            d = truncated Julian date (JD-2,400,000 days)
-#                            m = modified Julian date  (MJD = JD-2,400,000.5 days)
-#                            y = fractional Besselian year
-#   18    T177,F10.6       Error in T0. Units are the same as for T0.
-#   19    T188,F8.6        Eccentricity (e).
-#   20    T197,F8.6        Error in e.
-#   21    T206,F8.4,A1     Longitude of periastron (omega), in degrees, reckoned 
-#                          from the node as listed. If the published omega value 
-#                          is later determined to fall in the wrong quadrant, the 
-#                          value is flipped by 180deg; a letter "q" indicates the 
-#                          quadrant has been corrected.
-#   22    T215,F8.4        Error in omega.
-#   23    T224,I4          Equinox, if any, to which the node refers.
+    (117, 125, 'f', 'semi_major_error'),    # Error in a. Units are the same as for a.
+    (126, 134, 'f', 'inclination'),         # Inclination (i), in degrees.
+    (135, 143, 'f', 'inclination_error'),   # Error in i.
+    (144, 152, 'f', 'node'),                # Node (Omega), in degrees. An identified ascending node 
+                                                # is indicated by an asterisk following the value. If 
+                                                # the ascending node is later determined to off by 
+                                                # 180deg, it is flipped, and a "q" code added to 
+                                                # indicate the change. 
+    (154, 162, 'f', 'node_error'),          #  Error in Omega.
+    (163, 174, 'f', 'time_periastron'),          # The time of periastron passage (T0) 
+    (175, 175, 's', 'time_periastron_units'),    # Perisastron code for units:
+                                                # c = centuries (fractional year / 100; used only for 
+                                                #   alp Cen + Proxima Cen) 
+                                                # d = truncated Julian date (JD-2,400,000 days)
+                                                # m = modified Julian date  (MJD = JD-2,400,000.5 days)
+                                                # y = fractional Besselian year
+    (177, 187, 'f', 'time_periastron_error'),    # Error in T0. Units are the same as for T0.
+    (188, 196, 'f', 'eccentricity'),        # Eccentricity (e).
+    (197, 205, 'f', 'eccentricity_error'),  # Error in e.
+    (206, 214, 'f', 'lon_periastron'),      # Longitude of periastron (omega), in degrees, reckoned 
+                                            # from the node as listed. If the published omega value 
+                                            # is later determined to fall in the wrong quadrant, the 
+                                            # value is flipped by 180deg; a letter "q" indicates the 
+                                            # quadrant has been corrected.
+    (215, 223, 'f', 'lon_periastron_error'),# Error in omega.
+    (224, 228, 'i', 'equinox'),             # Equinox, if any, to which the node refers.
 
     (229, 233, 'i', 'last_date'),           # Date of the last observation used in the orbit 
                                             # calculation, if published.
@@ -72,9 +73,9 @@ sixth_orbital_columns = [
                                                 #  by the present scheme) and a grade of 9 indicates an
                                                 #  astrometric binary (also lacking rho and theta data).
 
-#   26    T236,A1          A flag "n" to any notes for this system. 
-#   27    T238,A8          A code for the reference (usually based on the name of 
-#                          the first author and the date of publication).
+    (236, 236, 's', 'notes'),               # A flag "n" to any notes for this system. 
+    (238, 245, 's', 'reference'),           # A code for the reference (usually based on the name of 
+                                                # the first author and the date of publication).
 
     (247, 265, 's', 'orbit_image'),          # Name of image file (png format) illustrating orbit and
                                                 # all associated measures in the Washington Double Star 
